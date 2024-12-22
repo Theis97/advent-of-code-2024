6676cb172455e747047c4b83023bcf0050c74af4
Susan Theis <sttheis97@gmail.com>
2024-12-21 22:39:58 -0600

    Day 16 WIP

--- src/routes/advent-of-code-2024/day-16.lazy.tsx
+++ src/routes/advent-of-code-2024/day-16.lazy.tsx
@@ -5,11 +5,83 @@ export const Route = createLazyFileRoute("/advent-of-code-2024/day-16")({
   component: DaySixteen,
 });
 
+enum Direction {
+  Up,
+  Right,
+  Down,
+  Left,
+}
+
+const turn = (direction: Direction, clockwise: boolean) => {
+  if (clockwise) {
+    return direction + (1 % 4);
+  } else {
+    return direction === Direction.Up ? Direction.Left : direction - 1;
+  }
+};
+
+type Node = {
+  coord: [number, number];
+  facing: Direction;
+};
+
+type Edge = {
+  node: Node;
+  weight: number;
+};
+
 function DaySixteen() {
   const [input, setInput] = useState("");
 
   const inputLines = input.trim().split("\n");
-  console.log(inputLines);
+
+  let map: string[][] = [];
+  let startTile: [number, number] = [-1, -1];
+  let endTile: [number, number] = [-1, -1];
+  for (let i = 0; i < inputLines.length; i++) {
+    map.push([]);
+    for (let j = 0; j < inputLines[i].length; j++) {
+      map[i].push(inputLines[i][j]);
+      if (inputLines[i][j] === "S") {
+        startTile = [i, j];
+      } else if (inputLines[i][j] === "E") {
+        endTile = [i, j];
+      }
+    }
+  }
+
+  // construct a graph where each node consists of a tile coordinate and a facing direction.
+  // edges are weighted based on the score incurred by moving from one position to another.
+  let exploredTiles: [number, number][] = [];
+  let tilesToExplore: [number, number][] = [startTile];
+  let graph = new Map<Node, Edge[]>();
+  while (tilesToExplore.length > 0 && startTile[0] !== -1) {
+    let nextTilesToExplore: [number, number][] = [];
+    tilesToExplore.forEach((tile) => {
+      // create 4 nodes for each direction you can face on this tile
+      for (let i = 0; i < 4; i++) {
+        graph.set({ coord: tile, facing: i }, [
+          { node: { coord: tile, facing: turn(i, true) }, weight: 1000 },
+          { node: { coord: tile, facing: turn(i, false) }, weight: 1000 },
+        ]);
+      }
+
+      if (map[tile[0] - 1][tile[0]] !== "#") {
+        // Tile above is open
+      } else if (map[tile[0]][tile[0] + 1] !== "#") {
+        // Tile to the right is open
+      } else if (map[tile[0] + 1][tile[0]] !== "#") {
+        // Tile below is open
+      } else if (map[tile[0]][tile[0] + 1] !== "#") {
+        // Tile to the left is open
+      }
+
+      exploredTiles.push(tile);
+    });
+
+    // TODO: Actually finish implementing this loop
+    break;
+  }
 
   return (
     <div>
